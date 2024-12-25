e45aaa188058aa859a6edd8962f2b2e74c32478b
KarthikRaja0007 <karthik.raja.007c@gmail.com>
2024-12-25 21:58:07 +0530

    commit

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -2,6 +2,7 @@ import { Webhook } from 'svix';
 import { headers } from 'next/headers';
 import { clerkClient } from '@clerk/nextjs/server';
 import { createOrUpdateUser, deleteUser } from '@/lib/actions/user';
+import User from '@/lib/models/user.model';
 
 
 export async function POST(req) {

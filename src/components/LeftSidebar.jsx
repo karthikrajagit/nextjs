ad22b80916ada6b0c016f65b4bc37c9090456969
KarthikRaja0007 <karthik.raja.007c@gmail.com>
2025-01-02 14:14:26 +0530

    commit

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -1,53 +1,39 @@
-'use client'
-
-import React from 'react'
-import { FaXTwitter } from 'react-icons/fa6';
-import { HiHome } from 'react-icons/hi';
 import Link from 'next/link';
-
-import { SignInButton, SignOutButton, SignedIn, SignedOut } from '@clerk/nextjs';
+import { GiBrain } from "react-icons/gi";
+import { HiHome } from 'react-icons/hi';
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+} from '@clerk/nextjs';
 import MiniProfile from './MiniProfile';
-
-export default function LeftSidebar() {
+export default function Sidebar() {
   return (
-    <div>
-      <div className="flex flex-col p-3 gap-4">
-        <Link href="/">
-          <FaXTwitter
-            size={30}
-            className="w-16 h-16 cursor-pointer p-3 hover:bg-slate-200 rounded-full transition-all duration-300"
-          />
+    <div className='flex flex-col p-3 justify-between h-screen items-center'>
+      <div className='flex flex-col gap-4 p-3'>
+        <Link href='/'>
+          <GiBrain className='w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200 ' />
         </Link>
         <Link
-          href="/"
-          className="flex items-center p-3 hover:bg-slate-200 rounded-full transition-all duration-300 gap-2 w-fit"
+          href='/'
+          className='flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit'
         >
-          <HiHome className="w-7 h-7" />
-          <span className="font-bold hidden xl:inline">Home</span>
+          <HiHome className='w-7 h-7' />
+          <span className='font-bold hidden xl:inline'>Home</span>
         </Link>
-
-        {/* Conditional buttons for Sign In/Sign Out */}
-        <SignedIn>
-          <SignOutButton>
-            <button className="bg-green-500 hover:bg-green-400 rounded-full text-white font-bold transition-all duration-300 w-48 h-9 shadow-md hidden xl:inline">
-              Sign Out
-            </button>
-          </SignOutButton>
-        </SignedIn>
-        <SignedOut>
-          <SignInButton>
-            <button className="bg-green-500 hover:bg-green-400 rounded-full text-white font-bold transition-all duration-300 w-48 h-9 shadow-md hidden xl:inline">
-              Sign In
-            </button>
-          </SignInButton>
-        </SignedOut>
+        <button className='bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'>
+          <SignedIn>
+            <SignOutButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+        </button>
       </div>
-
-      <div className="mt-10">
-        <SignedIn>
-          <MiniProfile />
-        </SignedIn>
-      </div>
+      <SignedIn>
+        <MiniProfile />
+      </SignedIn>
     </div>
   );
 }
--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -1,31 +1,34 @@
-'use client'
+'use client';
 
-import React, { useState } from 'react'
-import { useRouter } from 'next/navigation'
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import News from './News';
 
-
 export default function RightSidebar() {
-  const router = useRouter()
   const [input, setInput] = useState('');
+  const router = useRouter();
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(input.trim==='' || !input) return;
+    if (!input.trim()) return;
     router.push(`/search/${input}`);
     setTimeout(() => {
       router.refresh();
-    }, 100)
-  }
+    }, 100);
+  };
   return (
     <>
-      <div className='sticky top-0 bg-white p-2'>
+      <div className='sticky top-0 bg-white py-2'>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Search" value={input} onChange={(e) => setInput(e.target.value)}
-          className='rounded-3xl bg-gray-200 border border-gray-300 text-sm w-full px-4 py-3'
+          <input
+            type='text'
+            placeholder='Search'
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className='bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2'
           />
         </form>
       </div>
-      <News/>
+      <News />
     </>
-  )
+  );
 }
